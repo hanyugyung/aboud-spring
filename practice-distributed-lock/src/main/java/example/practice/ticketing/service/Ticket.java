46e6b28585c883161fbba4d6e663d70a1e86893f
han60 <dbruddl20202@naver.com>
2022-08-27 23:27:34 +0900

    - embedded redis 의존성을 추가하며 발생한 logback 과 slf4j-simple 충돌 방지를 위해 slf4j-simple 제거 - embedded redis 설정 추가(보통은 클래스위에 @Profile 설정해준다 - 스레드를 통해 동시 접근 구현

--- practice-distributed-lock/src/main/java/example/practice/ticketing/service/Ticket.java
+++ practice-distributed-lock/src/main/java/example/practice/ticketing/service/Ticket.java
@@ -2,6 +2,7 @@ package example.practice.ticketing.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @AllArgsConstructor
@@ -13,5 +14,6 @@ public class Ticket {
 
     public void sell() {
         this.quantity -= 1;
+        System.out.println("현재 남은 티켓 개수 : " + this.quantity);
     }
 }
--- practice-distributed-lock/src/main/java/example/practice/ticketing/service/TicketingService.java
+++ practice-distributed-lock/src/main/java/example/practice/ticketing/service/TicketingService.java
@@ -1,7 +1,6 @@
 package example.practice.ticketing.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
@@ -22,23 +21,25 @@ public class TicketingService {
         this.ticket = new Ticket(quantity);
     }
 
-    public int getCurrentTicketQuantity(){
+    public int getCurrentTicketQuantity() {
         return this.ticket.getQuantity();
     }
 
-    public void buyTicket() throws InterruptedException {
+    public void buyTicket() {
 
         RLock lock = redissonClient.getLock(key);
 
         try {
             // 최대 5초동안은 lock 을 기다려본다
             // 1초동안 lock 을 점유한다
-            if(!lock.tryLock(5, 1, TimeUnit.SECONDS)) {
+            if (!lock.tryLock(5, 1, TimeUnit.SECONDS)) {
                 // 5초가 지나도 lock 을 얻을 수 없는 경우
                 throw new IllegalStateException("재구매..");
             }
 
             this.ticket.sell();
+
+        } catch (Exception e) {
 
         } finally {
             lock.unlock();
