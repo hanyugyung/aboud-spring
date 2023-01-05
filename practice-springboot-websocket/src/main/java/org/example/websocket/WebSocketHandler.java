6bf521ade926ed7c34acf442499e60161db8a33a
han60 <dbruddl20202@naver.com>
2023-01-05 18:42:10 +0900

    spring boot websocket sample - stomp, sockJs 등 사용하지 않은 기본 예제 작성 - 웹소켓에 연결된 자신을 제외한 세션들에게 메시지 전송 예제

---
+++ practice-springboot-websocket/src/main/java/org/example/websocket/WebSocketHandler.java
@@ -0,0 +1,40 @@
+package org.example.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.dto.Connection;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("afterConnectionEstablished , sessionId = {}", session.getId());
+        Connection.addConnection(session);
+    }
+
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        Connection.getConnections().forEach((sessionId, sessionValue) -> {
+            if (!sessionId.equals(session.getId())) {
+                try {
+                    sessionValue.sendMessage(message);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("afterConnectionClosed , sessionId = {}", session.getId());
+    }
+}
